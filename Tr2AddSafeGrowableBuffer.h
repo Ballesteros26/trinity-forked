57db5870d940370c5f7243fdfeecbb1e7caeb745
filipppavlov <filipppavlov@users.noreply.github.com>
2015-10-07 15:28:25 +0000

    Moved Tr2AddSafeGrowableBuffer into a separate file and fixed uninitialized variables
    Binaries submitted in 969222
    Build time : 9 minutes 9.105000 seconds
    Branch : //eve/branches/development/MAINLINE/
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 969209]

---
+++ Tr2AddSafeGrowableBuffer.h
@@ -0,0 +1,164 @@
+#pragma once
+#ifndef Tr2AddSafeGrowableBuffer_H
+#define Tr2AddSafeGrowableBuffer_H
+
+
+// --------------------------------------------------------------------------------------
+// Description:
+//   Helper class to store a growable array of objects. A super-limited version of 
+//   std::vector, but it allows appending new objects in parallel. All other operations
+//   are not thread safe.
+// --------------------------------------------------------------------------------------
+template<typename T>
+class Tr2AddSafeGrowableBuffer
+{
+public:
+	Tr2AddSafeGrowableBuffer( uint32_t initialCount, const char* allocationName );
+	void Clear();
+	bool Add( const T& element, const char* allocationName );
+	uint32_t GetCount() const;
+	const T* GetData() const;
+	T* GetData();
+
+	const T& operator[]( size_t index ) const;
+	T& operator[]( size_t index );
+	const T* begin() const;
+	T* begin();
+	const T* end() const;
+	T* end();
+private:
+	CcpMallocBuffer m_data;
+	CcpAtomic<uint32_t> m_count;
+	CcpAtomic<uint32_t> m_capacity;
+	CcpAtomic<uint32_t> m_dataUsed;
+};
+
+template <typename T>
+const T* begin( const Tr2AddSafeGrowableBuffer<T>& buffer )
+{
+	return buffer.begin();
+}
+
+template <typename T>
+T* begin( Tr2AddSafeGrowableBuffer<T>& buffer )
+{
+	return buffer.begin();
+}
+
+template <typename T>
+const T* end( const Tr2AddSafeGrowableBuffer<T>& buffer )
+{
+	return buffer.GetData() + buffer.GetCount();
+}
+
+template <typename T>
+T* end( Tr2AddSafeGrowableBuffer<T>& buffer )
+{
+	return buffer.GetData() + buffer.GetCount();
+}
+
+
+
+
+
+template<typename T>
+Tr2AddSafeGrowableBuffer<T>::Tr2AddSafeGrowableBuffer( uint32_t initialCount, const char* allocationName )
+	:m_capacity( initialCount ),
+	m_count( 0 ),
+	m_dataUsed( 0 )
+{
+	m_data.resize( allocationName, m_capacity * sizeof( T ) );
+}
+
+template<typename T>
+void Tr2AddSafeGrowableBuffer<T>::Clear()
+{
+	m_count = 0;
+}
+
+template<typename T>
+bool Tr2AddSafeGrowableBuffer<T>::Add( const T& element, const char* allocationName )
+{
+	uint32_t index = m_count++;
+	while( true )
+	{
+		if( index == m_capacity )
+		{
+			while( m_dataUsed )
+			{
+			}
+			m_data.resize( allocationName, m_capacity * 2 * sizeof( T ) );
+			m_capacity = m_capacity * 2;
+		}
+		else if( index < m_capacity )
+		{
+			break;
+		}
+	}
+
+	m_dataUsed = 1;
+	if( !m_data.get() )
+	{
+		m_dataUsed = 0;
+		return false;
+	}
+	reinterpret_cast<T*>( m_data.get() )[index] = element;
+	m_dataUsed = 0;
+	return true;
+}
+
+template<typename T>
+uint32_t Tr2AddSafeGrowableBuffer<T>::GetCount() const
+{
+	return m_count;
+}
+
+template<typename T>
+const T* Tr2AddSafeGrowableBuffer<T>::GetData() const
+{
+	return reinterpret_cast<const T*>( m_data.get() );
+}
+
+template<typename T>
+T* Tr2AddSafeGrowableBuffer<T>::GetData()
+{
+	return reinterpret_cast<T*>( m_data.get() );
+}
+
+template<typename T>
+const T& Tr2AddSafeGrowableBuffer<T>::operator[]( size_t index ) const
+{
+	return GetData()[index];
+}
+
+template<typename T>
+T& Tr2AddSafeGrowableBuffer<T>::operator[]( size_t index )
+{
+	return GetData()[index];
+}
+
+template<typename T>
+const T* Tr2AddSafeGrowableBuffer<T>::begin() const
+{
+	return GetData();
+}
+
+template<typename T>
+T* Tr2AddSafeGrowableBuffer<T>::begin()
+{
+	return GetData();
+}
+
+template<typename T>
+const T* Tr2AddSafeGrowableBuffer<T>::end() const
+{
+	return GetData() + GetCount();
+}
+
+template<typename T>
+T* Tr2AddSafeGrowableBuffer<T>::end()
+{
+	return GetData() + GetCount();
+}
+
+#endif
--- Tr2LightManager.h
+++ Tr2LightManager.h
@@ -11,33 +11,11 @@
 #include "Tr2DeviceResource.h"
 #include "Tr2Variable.h"
 #include "TriFrustum.h"
+#include "Tr2AddSafeGrowableBuffer.h"
 
 BLUE_DECLARE( Tr2Effect );
 BLUE_DECLARE( Tr2GpuBuffer );
 BLUE_DECLARE( Tr2GpuStructuredBuffer );
-
-
-// --------------------------------------------------------------------------------------
-// Description:
-//   Helper class to store a growable array of objects. A super-limited version of 
-//   std::vector, but it allows appending new objects in parallel. All other operations
-//   are not thread safe.
-// --------------------------------------------------------------------------------------
-template<typename T>
-class Tr2AddSafeGrowableBuffer
-{
-public:
-	Tr2AddSafeGrowableBuffer( uint32_t initialCount, const char* allocationName );
-	void Clear();
-	bool Add( const T& element, const char* allocationName );
-	uint32_t GetCount() const;
-	const T* GetData() const;
-private:
-	CcpMallocBuffer m_data;
-	CcpAtomic<uint32_t> m_count;
-	CcpAtomic<uint32_t> m_capacity;
-	CcpAtomic<uint32_t> m_dataUsed;
-};
 
 
 // --------------------------------------------------------------------------------------
@@ -99,63 +77,5 @@ private:
 	Tr2Variable m_indexBufferVariable;
 	TriFrustum m_frustum;
 };
-
-
-
-template<typename T>
-Tr2AddSafeGrowableBuffer<T>::Tr2AddSafeGrowableBuffer( uint32_t initialCount, const char* allocationName )
-	:m_capacity( initialCount )
-{
-	m_data.resize( allocationName, m_capacity * sizeof( T ) );
-}
-
-template<typename T>
-void Tr2AddSafeGrowableBuffer<T>::Clear()
-{
-	m_count = 0;
-}
-
-template<typename T>
-bool Tr2AddSafeGrowableBuffer<T>::Add( const T& element, const char* allocationName )
-{
-	uint32_t index = m_count++;
-	while( true )
-	{
-		if( index == m_capacity )
-		{
-			while( m_dataUsed )
-			{
-			}
-			m_data.resize( allocationName, m_capacity * 2 * sizeof( T ) );
-			m_capacity = m_capacity * 2;
-		}
-		else if( index < m_capacity )
-		{
-			break;
-		}
-	}
-
-	m_dataUsed = 1;
-	if( !m_data.get() )
-	{
-		m_dataUsed = 0;
-		return false;
-	}
-	reinterpret_cast<T*>( m_data.get() )[index] = element;
-	m_dataUsed = 0;
-	return true;
-}
-
-template<typename T>
-uint32_t Tr2AddSafeGrowableBuffer<T>::GetCount() const
-{
-	return m_count;
-}
-
-template<typename T>
-const T* Tr2AddSafeGrowableBuffer<T>::GetData() const
-{
-	return reinterpret_cast<const T*>( m_data.get() );
-}
 
 #endif
