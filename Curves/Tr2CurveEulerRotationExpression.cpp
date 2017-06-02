b0ab742c3d40540d653bbf8ecaac4204b813bb7f
filipppavlov <filipppavlov@users.noreply.github.com>
2017-06-02 10:34:49 +0000

    Convert Tr2.../Tri... curves to the new types https://jira.ccpgames.com/browse/GFX-2755
    Added Tr2CurveEulerRotationExpression type
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 1150409]

---
+++ Curves/Tr2CurveEulerRotationExpression.cpp
@@ -0,0 +1,305 @@
+////////////////////////////////////////////////////////////
+//
+//    Created:   June 2017
+//    Copyright: CCP 2017
+//
+
+#include "StdAfx.h"
+#include "Tr2CurveEulerRotationExpression.h"
+#include "include/TriMath.h"
+
+extern bool g_expressionCurveFakeRandom;
+
+
+namespace
+{
+	CcpMutex s_mutex( "Tr2CurveScalarExpression", "s_mutex", 1000 );
+
+	std::vector<const Tr2CurveEulerRotationExpression*> s_currentCurve;
+
+	// --------------------------------------------------------------------------------
+	float Fractal( float x, float alpha, float beta, float n )
+	{
+		return float( ( PerlinNoise1D( x + s_currentCurve.back()->GetRandomConstant(), alpha, beta, int( n + 0.5f ) ) + 1.0 ) / 2.0 );
+	}
+
+	// --------------------------------------------------------------------------------
+	float Noise( float x )
+	{
+		return float( ( PerlinNoise1D( x + s_currentCurve.back()->GetRandomConstant(), 1.0, 1.0, 1 ) + 1.0 ) / 2.0 );
+	}
+
+	// --------------------------------------------------------------------------------
+	float RandomConstant( float a, float b )
+	{
+		return ( ( b - a ) * s_currentCurve.back()->GetRandomConstant() ) + a;
+	}
+
+	// --------------------------------------------------------------------------------
+	float Random( float a, float b )
+	{
+		if( g_expressionCurveFakeRandom )
+		{
+			return ( ( b - a ) * 0.41f ) + a;
+		}
+		return ( ( b - a ) * ( (float)rand() / RAND_MAX ) ) + a;
+	}
+
+	// --------------------------------------------------------------------------------
+	float Input( float index )
+	{
+		return s_currentCurve.back()->GetInputValue( int32_t( index + 0.5f ) );
+	}
+
+	// --------------------------------------------------------------------------------
+	float InputAt( float index, float time )
+	{
+		return s_currentCurve.back()->GetInputValue( int32_t( index + 0.5f ), time );
+	}
+}
+
+// --------------------------------------------------------------------------------
+Tr2CurveEulerRotationExpression::Tr2CurveEulerRotationExpression( IRoot* lockobj )
+	:PARENTLOCK( m_inputs ),
+	m_currentValue( 0, 0, 0, 1 ),
+	m_timeScale( 1 ),
+	m_randomConstant( float( rand() ) / RAND_MAX ),
+	m_input1( 0 ),
+	m_input2( 0 ),
+	m_input3( 0 ),
+	m_input4( 0 )
+{
+	for( size_t i = 0; i < 3; ++i )
+	{
+		auto& parser = m_expressionParsers[i];
+
+		parser.DefineFun( "fractal", &Fractal, false );
+		parser.DefineFun( "noise", &Noise, false );
+		parser.DefineFun( "randomConstant", &RandomConstant, false );
+		parser.DefineFun( "randconst", &RandomConstant, false );
+		parser.DefineFun( "random", &Random, false );
+		parser.DefineFun( "input", &Input, false );
+		parser.DefineFun( "inputAt", &InputAt, false );
+		parser.DefineFun( "clamp", &Clamp, false );
+		parser.DefineFun( "radians", &XMConvertToRadians, true );
+
+		parser.DefineVar( "input1", &m_input1 );
+		parser.DefineVar( "input2", &m_input2 );
+		parser.DefineVar( "input3", &m_input3 );
+		parser.DefineVar( "input4", &m_input4 );
+
+		parser.DefineVar( "time", &m_time );
+
+		parser.DefineConst( "pi", 3.1415926f );
+		parser.DefineConst( "pi2", 2.0f * 3.1415926f );
+	}
+}
+
+// --------------------------------------------------------------------------------
+bool Tr2CurveEulerRotationExpression::Initialize()
+{
+	for( size_t i = 0; i < 3; ++i )
+	{
+		if( !m_expressions[i].empty() )
+		{
+			auto expression = m_expressions[i];
+			m_expressions[i] = "";
+			SetExpression( i, expression );
+		}
+	}
+	return true;
+
+}
+
+// --------------------------------------------------------------------------------
+void Tr2CurveEulerRotationExpression::UpdateValue( double time )
+{
+	m_currentValue = GetValue( time );
+}
+
+// --------------------------------------------------------------------------------
+Quaternion Tr2CurveEulerRotationExpression::GetValue( double time ) const
+{
+	Vector3 result( 0, 0, 0 );
+	float* components = &result.x;
+
+	m_time = float( time / m_timeScale );
+
+	for( size_t i = 0; i < 3; ++i )
+	{
+		if( m_expressions[i].empty() )
+		{
+			continue;
+		}
+
+		CcpAutoMutex lock( s_mutex );
+		s_currentCurve.push_back( this );
+
+		try
+		{
+			components[i] = m_expressionParsers[i].Eval();
+		}
+		catch( const mu::Parser::exception_type& )
+		{
+			components[i] = 0;
+		}
+		s_currentCurve.pop_back();
+	}
+	return Quaternion( XMQuaternionRotationRollPitchYaw( result.y, result.x, result.z ) );
+
+}
+
+// --------------------------------------------------------------------------------
+std::string Tr2CurveEulerRotationExpression::GetExpression( size_t index ) const
+{
+	return m_expressions[index];
+}
+
+// --------------------------------------------------------------------------------
+void Tr2CurveEulerRotationExpression::SetExpression( size_t index, const std::string& expression )
+{
+	if( expression.empty() )
+	{
+		m_expressions[index] = expression;
+		return;
+	}
+	m_expressionParsers[index].SetExpr( expression );
+
+	CcpAutoMutex lock( s_mutex );
+	s_currentCurve.push_back( this );
+
+	try
+	{
+		m_expressionParsers[index].Eval();
+	}
+	catch( const mu::Parser::exception_type& e )
+	{
+		s_currentCurve.pop_back();
+		CCP_LOGERR( "Tr2CurveEulerRotationExpression::SetExpression invalid expression \"%s\": %s", expression.c_str(), e.GetMsg().c_str() );
+		m_expressionParsers[index].SetExpr( m_expressions[index] );
+		return;
+	}
+	s_currentCurve.pop_back();
+	m_expressions[index] = expression;
+}
+
+// --------------------------------------------------------------------------------
+std::string Tr2CurveEulerRotationExpression::GetExpressionYaw() const
+{
+	return GetExpression( 0 );
+}
+
+// --------------------------------------------------------------------------------
+void Tr2CurveEulerRotationExpression::SetExpressionYaw( const std::string& expression )
+{
+	SetExpression( 0, expression );
+}
+
+// --------------------------------------------------------------------------------
+std::string Tr2CurveEulerRotationExpression::GetExpressionPitch() const
+{
+	return GetExpression( 1 );
+}
+
+// --------------------------------------------------------------------------------
+void Tr2CurveEulerRotationExpression::SetExpressionPitch( const std::string& expression )
+{
+	SetExpression( 1, expression );
+}
+
+// --------------------------------------------------------------------------------
+std::string Tr2CurveEulerRotationExpression::GetExpressionRoll() const
+{
+	return GetExpression( 2 );
+}
+
+// --------------------------------------------------------------------------------
+void Tr2CurveEulerRotationExpression::SetExpressionRoll( const std::string& expression )
+{
+	SetExpression( 2, expression );
+}
+
+// --------------------------------------------------------------------------------
+float Tr2CurveEulerRotationExpression::GetRandomConstant() const
+{
+	if( g_expressionCurveFakeRandom )
+	{
+		return 0.21f;
+	}
+	else
+	{
+		return m_randomConstant;
+	}
+}
+
+// --------------------------------------------------------------------------------
+float Tr2CurveEulerRotationExpression::GetInputValue( int index ) const
+{
+	if( index < 0 || index >= int( m_inputs.size() ) )
+	{
+		return 0;
+	}
+	return const_cast<ITriScalarFunction*>( m_inputs[index] )->GetValueAt( m_time );
+}
+
+// --------------------------------------------------------------------------------
+float Tr2CurveEulerRotationExpression::GetInputValue( int index, float time ) const
+{
+	if( index < 0 || index >= int( m_inputs.size() ) )
+	{
+		return 0;
+	}
+	return const_cast<ITriScalarFunction*>( m_inputs[index] )->GetValueAt( time );
+}
+
+// --------------------------------------------------------------------------------
+Quaternion* Tr2CurveEulerRotationExpression::Update( Quaternion* in, Be::Time time )
+{
+	*in = m_currentValue = GetValue( TimeAsDouble( time ) );
+	return in;
+}
+
+// --------------------------------------------------------------------------------
+Quaternion* Tr2CurveEulerRotationExpression::Update( Quaternion* in, double time )
+{
+	*in = m_currentValue = GetValue( time );
+	return in;
+}
+
+// --------------------------------------------------------------------------------
+Quaternion* Tr2CurveEulerRotationExpression::GetValueAt( Quaternion* in, Be::Time time )
+{
+	*in = GetValue( TimeAsDouble( time ) );
+	return in;
+}
+
+// --------------------------------------------------------------------------------
+Quaternion* Tr2CurveEulerRotationExpression::GetValueAt( Quaternion* in, double time )
+{
+	*in = GetValue( time );
+	return in;
+}
+
+// --------------------------------------------------------------------------------
+Quaternion* Tr2CurveEulerRotationExpression::GetValueDotAt( Quaternion* in, Be::Time time )
+{
+	return in;
+}
+
+// --------------------------------------------------------------------------------
+Quaternion* Tr2CurveEulerRotationExpression::GetValueDotAt( Quaternion* in, double time )
+{
+	return in;
+}
+
+// --------------------------------------------------------------------------------
+Quaternion* Tr2CurveEulerRotationExpression::GetValueDoubleDotAt( Quaternion* in, Be::Time time )
+{
+	return in;
+}
+
+// --------------------------------------------------------------------------------
+Quaternion* Tr2CurveEulerRotationExpression::GetValueDoubleDotAt( Quaternion* in, double time )
+{
+	return in;
+}
---
+++ Curves/Tr2CurveEulerRotationExpression_Blue.cpp
@@ -0,0 +1,97 @@
+////////////////////////////////////////////////////////////
+//
+//    Created:   June 2017
+//    Copyright: CCP 2017
+//
+
+#include "StdAfx.h"
+#include "Tr2CurveEulerRotationExpression.h"
+
+BLUE_DEFINE( Tr2CurveEulerRotationExpression );
+
+const Be::ClassInfo* Tr2CurveEulerRotationExpression::ExposeToBlue()
+{
+	EXPOSURE_BEGIN( Tr2CurveEulerRotationExpression, ":jessica-icon: tree/trirotationcurve.png" )
+		MAP_INTERFACE( Tr2CurveEulerRotationExpression )
+		MAP_INTERFACE( ITriQuaternionFunction )
+		MAP_INTERFACE( ITriFunction )
+		MAP_INTERFACE( IInitialize )
+
+		MAP_ATTRIBUTE(
+			"name",
+			m_name,
+			"",
+			Be::READWRITE | Be::PERSIST )
+
+		MAP_ATTRIBUTE(
+			"expressionYaw",
+			m_expressions[0],
+			"Curve expression for x component",
+			Be::PERSISTONLY )
+		MAP_PROPERTY(
+			"expressionYaw",
+			GetExpressionYaw,
+			SetExpressionYaw,
+			"Curve expression for x component" )
+		MAP_ATTRIBUTE(
+			"expressionPitch",
+			m_expressions[1],
+			"Curve expression for y component",
+			Be::PERSISTONLY )
+		MAP_PROPERTY(
+			"expressionPitch",
+			GetExpressionPitch,
+			SetExpressionPitch,
+			"Curve expression for y component" )
+		MAP_ATTRIBUTE(
+			"expressionRoll",
+			m_expressions[2],
+			"Curve expression for z component",
+			Be::PERSISTONLY )
+		MAP_PROPERTY(
+			"expressionRoll",
+			GetExpressionRoll,
+			SetExpressionRoll,
+			"Curve expression for z component" )
+
+		MAP_ATTRIBUTE(
+			"currentValue",
+			m_currentValue,
+			"Curve value after the last update",
+			Be::READ )
+		MAP_ATTRIBUTE(
+			"inputs",
+			m_inputs,
+			"Scalar curve inputs",
+			Be::READ | Be::PERSIST )
+
+		MAP_ATTRIBUTE(
+			"input1",
+			m_input1,
+			"Input variable",
+			Be::READWRITE | Be::PERSIST )
+		MAP_ATTRIBUTE(
+			"input2",
+			m_input2,
+			"Input variable",
+			Be::READWRITE | Be::PERSIST )
+		MAP_ATTRIBUTE(
+			"input3",
+			m_input3,
+			"Input variable",
+			Be::READWRITE | Be::PERSIST )
+		MAP_ATTRIBUTE(
+			"input4",
+			m_input4,
+			"Input variable",
+			Be::READWRITE | Be::PERSIST )
+
+		MAP_METHOD_AND_WRAP(
+			"GetValueAt",
+			GetValue,
+			"Returns curve value at specified time\n"
+			":param time: input time"
+		)
+
+		EXPOSURE_END()
+}
