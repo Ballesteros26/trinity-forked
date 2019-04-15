c0bacb8773d75cc11f65c1e29e5b05be7acfd19b
ccpnobody <55136070+ccpnobody@users.noreply.github.com>
2019-04-15 14:52:18 +0000

    Fixing more mac build errors
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 1492061]

--- Lights/Tr2SpotLight.cpp
+++ Lights/Tr2SpotLight.cpp
@@ -24,8 +24,8 @@ void Tr2SpotLight::RenderDebugInfo( Tr2DebugRenderer& renderer, const Matrix& wo
 	auto selectedColor = baseColor + Color( 0.0, 0.0, 0.0, 0.1 );
 
 	Vector3 end = Transform( Vector3( 0.0, 0.0, m_lightData.radius ), RotationMatrix( m_lightData.rotation ) ).GetXYZ();
-	end = Transform( end + m_lightData.position, worldMatrix ).GetXYZ();
-	Vector3 start = Transform( m_lightData.position, worldMatrix );
+	end = TransformCoord( end + m_lightData.position, worldMatrix );
+	Vector3 start = TransformCoord( m_lightData.position, worldMatrix );
 	float outerConeRadius = tan( TRI_2PI * m_lightData.outerAngle / 360.f ) * m_lightData.radius;
 	float innerConeRadius = tan( TRI_2PI * m_lightData.innerAngle / 360.f ) * m_lightData.radius;
 	renderer.DrawCone( this, end, start, outerConeRadius, 10, Tr2DebugRenderer::Solid, Tr2DebugColor( selectedColor, baseColor ) );
